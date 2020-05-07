4a41129f582bc95bcf3c7b90bb65520a3b7522cf
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-05-07 17:45:26 -0400

    getUser function updated to create GitHub query

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,11 @@
+// constructor api with one function getUser; getUser passes one parameter username  
 const api = {
   getUser(username) {
-
-  }
+    // parameter passed to retrieve repos of github user
+    const queryUrl = `https://api.github.com/users/${username}`;
+    return queryUrl;
+    //console.log(queryUrl);
+  }  
 };
 
 module.exports = api;
